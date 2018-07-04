6026c409b914e6c3d08a2c772d56dfd8f10cd1cd
Anton Iskov <iskov.anton@yandex.com>
2018-07-04 22:09:48 +0200

    changes

--- app/app.js
+++ app/app.js
@@ -2,33 +2,25 @@ var myCompany = angular.module("myCompany", []);
 
 myCompany.controller("myCompanyController", [
   "$scope",
-  function($scope) {
+  function ($scope) {
     $scope.people = [
       {
         name: "",
         email: ""
       }
     ];
-   
-  
 
-   $scope.$watch("people", function(newValue, oldValue) {
-        if(newValue) {
-          console.log("new value");
-          var last = $scope.people[$scope.people.length-1];
-            if(last.name || last.email){
-              console.log("if");
-              $scope.people.push({
-                name: "",
-                email:""
-              });
-            }
-        }     
-      else {
-        console.log("no changes");
-      }
-    });
+    var onContactsChange = function (newValue, oldValue) {
+        var last = $scope.people[$scope.people.length - 1];
+        if (! last.name && ! last.email) return;
 
-    
+        console.log("Adding new line (" + $scope.people.length + ")");
+        $scope.people.push({
+          name: "",
+          email: ""
+        });
+    };
+
+    $scope.$watch("people", onContactsChange, true);
   }
 ]);
